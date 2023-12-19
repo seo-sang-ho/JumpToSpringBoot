75e5bc1eca8451e74142fd66281b3bbd91a8a038
seo-sang-ho <rocter2049@naver.com>
2023-12-19 11:33:08 +0900

    feat: add Quesiton,Answer entity

---
+++ src/main/java/org/example/answer/answer/entity/Answer.java
@@ -0,0 +1,25 @@
+package org.example.answer.answer.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.question.question.entity.Question;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
