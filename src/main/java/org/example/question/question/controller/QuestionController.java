6f1177fd1f81070c5a41200e01ad2c250b099cd5
seo-sang-ho <rocter2049@naver.com>
2023-12-19 14:20:01 +0900

    feat: thymeleaf dependency, QuestionController

---
+++ src/main/java/org/example/question/question/controller/QuestionController.java
@@ -0,0 +1,28 @@
+package org.example.question.question.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.question.question.entity.Question;
+import org.example.question.question.repository.QuestionRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+    public String list(Model model){
+
+        List<Question> questionList = questionRepository.findAll();
+
+        model.addAttribute("questionList",questionList);
+
+        return "question_list";
+    }
+}
