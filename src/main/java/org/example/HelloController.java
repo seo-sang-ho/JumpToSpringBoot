acddbc1d4926e0cc2e6c11d0d256a5a629439a70
seo-sang-ho <rocter2049@naver.com>
2023-12-19 16:02:11 +0900

    change: inteillj Ultimate version

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
     @GetMapping("/")
-    @ResponseBody
     public String root(){
         return "redirect:/question/list";
     }
