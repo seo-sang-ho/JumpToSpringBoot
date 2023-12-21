219f773407c794104f60ed754bfd01a7c03cf98b
seo-sang-ho <rocter2049@naver.com>
2023-12-21 15:41:16 +0900

    feat: add login,logout function

--- src/main/java/org/example/user/user/repository/UserRepository.java
+++ src/main/java/org/example/user/user/repository/UserRepository.java
@@ -4,6 +4,8 @@ import org.example.user.user.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
 }
---
+++ src/main/java/org/example/user/user/role/UserRole.java
@@ -0,0 +1,15 @@
+package org.example.user.user.role;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    private String value;
+
+    UserRole(String value) {
+        this.value = value;
+    }
+}
