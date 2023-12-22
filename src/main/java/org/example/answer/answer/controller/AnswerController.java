93d8302954741922b8c8cd4010e7f4c6122598bb
seo-sang-ho <rocter2049@naver.com>
2023-12-22 17:20:20 +0900

    feat: add answer modify,delete

--- src/main/java/org/example/answer/answer/controller/AnswerController.java
+++ src/main/java/org/example/answer/answer/controller/AnswerController.java
@@ -5,19 +5,20 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.example.answer.answer.entity.Answer;
 import org.example.answer.answer.service.AnswerService;
+import org.example.question.question.controller.QuestionController;
 import org.example.question.question.entity.Question;
 import org.example.question.question.service.QuestionService;
 import org.example.user.user.entity.SiteUser;
 import org.example.user.user.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -47,5 +48,42 @@ public class AnswerController {
         }
         answerService.create(question,answerForm.content,siteUser);
         return String.format("redirect:/question/detail/%s",id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal) {
+        Answer answer = answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"수정권한이 없습니다.");
+        }
+        answerForm.setContent(answer.getContent());
+        return "answer_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
+                                 Principal principal, @PathVariable("id") Integer id) {
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+        Answer answer = answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        answerService.modify(answer,answerForm.getContent());
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String answerDelete(Principal principal, @PathVariable("id") Integer id){
+        Answer answer = answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"삭제 권한이 없습니다.");
+        }
+        answerService.delete(answer);
+        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
     }
 }
--- src/main/java/org/example/answer/answer/service/AnswerService.java
+++ src/main/java/org/example/answer/answer/service/AnswerService.java
@@ -4,11 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.example.answer.answer.entity.Answer;
 import org.example.answer.answer.repository.AnswerRepository;
 import org.example.question.question.entity.Question;
+import org.example.question.question.exception.DataNotFoundException;
 import org.example.user.user.entity.SiteUser;
 import org.springframework.cglib.core.Local;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +30,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id){
+        Optional<Answer> answer = answerRepository.findById(id);
+        if(answer.isPresent()){
+            return answer.get();
+        }else{
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        answerRepository.delete(answer);
     }
 }
