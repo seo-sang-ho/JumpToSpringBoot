1580173eb0dfdb44b774d325ec1fee18e8747e15
seo-sang-ho <rocter2049@naver.com>
2023-12-22 17:43:37 +0900

    feat: add Question recommend

--- src/main/java/org/example/answer/answer/controller/AnswerController.java
+++ src/main/java/org/example/answer/answer/controller/AnswerController.java
@@ -29,9 +29,10 @@ public class AnswerController {
     private final QuestionService questionService;
     private final AnswerService answerService;
     private final UserService userService;
+
     @Getter
     @Setter
-    public static class AnswerForm{
+    public static class AnswerForm {
         @NotEmpty(message = "댓글 내용은 필수입니다.")
         private String content;
     }
@@ -39,15 +40,15 @@ public class AnswerController {
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id,
-                               @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal){
+                               @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
         Question question = questionService.getQuestion(id);
         SiteUser siteUser = userService.getUser(principal.getName());
-        if(bindingResult.hasErrors()){
-            model.addAttribute("question",question);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
             return "question_detail";
         }
-        answerService.create(question,answerForm.content,siteUser);
-        return String.format("redirect:/question/detail/%s",id);
+        answerService.create(question, answerForm.content, siteUser);
+        return String.format("redirect:/question/detail/%s", id);
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -55,7 +56,7 @@ public class AnswerController {
     public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal) {
         Answer answer = answerService.getAnswer(id);
         if (!answer.getAuthor().getUsername().equals(principal.getName())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"수정권한이 없습니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
         answerForm.setContent(answer.getContent());
         return "answer_form";
@@ -64,7 +65,7 @@ public class AnswerController {
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/modify/{id}")
     public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
-                                 Principal principal, @PathVariable("id") Integer id) {
+                               Principal principal, @PathVariable("id") Integer id) {
         if (bindingResult.hasErrors()) {
             return "answer_form";
         }
@@ -72,18 +73,19 @@ public class AnswerController {
         if (!answer.getAuthor().getUsername().equals(principal.getName())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
-        answerService.modify(answer,answerForm.getContent());
+        answerService.modify(answer, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/delete/{id}")
-    public String answerDelete(Principal principal, @PathVariable("id") Integer id){
+    public String answerDelete(Principal principal, @PathVariable("id") Integer id) {
         Answer answer = answerService.getAnswer(id);
-        if(!answer.getAuthor().getUsername().equals(principal.getName())){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"삭제 권한이 없습니다.");
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
         }
         answerService.delete(answer);
-        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
+
 }
